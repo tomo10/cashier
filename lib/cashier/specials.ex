61dc2468957b278273b622c9c37894317d9eabb0
tomo <thce28@gmail.com>
2025-09-06 12:02:53 -0600

    Add other pricing rules to specials module

--- lib/cashier/specials.ex
+++ lib/cashier/specials.ex
@@ -2,7 +2,9 @@ defmodule Cashier.Specials do
   @zero Decimal.new("0")
 
   @rules [
-    &__MODULE__.rule_gr1_bogo/1
+    &__MODULE__.rule_gr1_bogo/1,
+    &__MODULE__.rule_cf1_three_for_two_thirds/1,
+    &__MODULE__.rule_sr1_bulk/1
   ]
 
   def calc_line_discounts(sku_item_map) do
@@ -14,8 +16,29 @@ defmodule Cashier.Specials do
 
   def rule_gr1_bogo(%{"GR1" => %Cashier.CartItem{quantity: q, list_unit_price: p}}) when q >= 2 do
     free = div(q, 2)
+
     Decimal.mult(p, Decimal.new(free))
   end
 
   def rule_gr1_bogo(_), do: @zero
+
+  def rule_sr1_bulk(%{"SR1" => %Cashier.CartItem{quantity: q, list_unit_price: p}}) when q >= 3 do
+    drop_to = Decimal.new("4.50")
+    per_unit_disc = Decimal.sub(p, drop_to)
+
+    Decimal.mult(per_unit_disc, Decimal.new(q))
+  end
+
+  def rule_sr1_bulk(_), do: @zero
+
+  def rule_cf1_three_for_two_thirds(%{"CF1" => %Cashier.CartItem{quantity: q, list_unit_price: p}})
+      when q >= 3 do
+    two_thirds = Decimal.div(Decimal.new(2), Decimal.new(3))
+    new_unit_price = Decimal.mult(p, two_thirds)
+    per_unit_disc = Decimal.sub(p, new_unit_price)
+
+    Decimal.mult(per_unit_disc, Decimal.new(q))
+  end
+
+  def rule_cf1_three_for_two_thirds(_), do: @zero
 end
